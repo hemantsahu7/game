6751b3feb033253307de0429bbc19a8300810cbe
hemantsahu7 <hs88901972@gmail.com>
2025-03-09 17:36:26 +0530

    corrected the gameserver link in io socket

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const WEAPON_OFFSET = {
   z: 0.8,
 };
 
-const socket = io("https://game-server-smoky.vercel.app/", {
+const socket = io("https://game-server-smoky.vercel.app", {
   withCredentials: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
